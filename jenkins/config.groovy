0b01952973ad8966f4c4ceab9a15887087b9aa2e
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-15 23:40:50 +0100

    Set PS4 in Jenkins
    To get better output

--- jenkins/config.groovy
+++ jenkins/config.groovy
@@ -29,3 +29,35 @@ instance.save()
 // Quiet period
 instance = Jenkins.getInstance()
 instance.setQuietPeriod(0)
+
+// Env vars
+import hudson.EnvVars;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.NodeProperty;
+import hudson.slaves.NodePropertyDescriptor;
+import hudson.util.DescribableList;
+import jenkins.model.Jenkins;
+
+public createGlobalEnvironmentVariables(String key, String value) {
+
+  DescribableList<NodeProperty<?>, NodePropertyDescriptor> globalNodeProperties =
+    instance.getGlobalNodeProperties();
+  List<EnvironmentVariablesNodeProperty> envVarsNodePropertyList =
+     globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class);
+
+  EnvironmentVariablesNodeProperty newEnvVarsNodeProperty = null;
+  EnvVars envVars = null;
+
+  if (envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0) {
+    newEnvVarsNodeProperty =
+      new hudson.slaves.EnvironmentVariablesNodeProperty();
+    globalNodeProperties.add(newEnvVarsNodeProperty);
+    envVars = newEnvVarsNodeProperty.getEnvVars();
+  } else {
+    envVars = envVarsNodePropertyList.get(0).getEnvVars();
+  }
+  envVars.put(key, value)
+  instance.save()
+}
+
+createGlobalEnvironmentVariables('PS4','+R-HUB-R-HUB-R-HUB')
