2218195b5c6c9677d471114c8a7b7e6b94d67108
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-16 11:35:06 +0100

    Add cron service
    This is supposed to run periodic activities. Right
    now it cleans the old Jenkins jobs.

---
+++ cron/Dockerfile
@@ -0,0 +1,17 @@
+
+FROM node:8
+
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are
+# copied where available (npm@5+)
+
+COPY package*json ./
+
+RUN npm install
+
+## Include the app's source code
+COPY . .
+
+CMD [ "npm", "start"  ]
---
+++ cron/index.js
@@ -0,0 +1,82 @@
+var CronJob = require('cron').CronJob;
+var jenkins = require('jenkins');
+var async = require('async');
+
+var jenkins_url = process.env.JENKINS_URL ||
+    'http://admin:2eea3b927d4ee23e@jenkins.rhub.me:8080';
+
+// Time limit to delete job
+var TIME_LIMIT = 1000 /* ms */ * 60 /* s */ * 60 /* min */ * 24 * 3;
+// How often to run the job reaper, once an hour, at **:42:42
+var CRON_JOB_REAPER = '48 45 * * * *';
+
+var job = new CronJob(CRON_JOB_REAPER, function() {
+
+    console.log("Running Jenkins job reaper");
+    var jen = jenkins({ baseUrl: jenkins_url, crumbIssuer: true });
+    jen.job.list(function(err, data) {
+	if (err) { console.log('Cannot get Jenkins job list'); return; }
+	async.eachLimit(
+	    data,
+	    3,
+	    function(job, cb) { delete_if_old(jen, job, cb); }
+	);
+    });
+
+}, null, true, 'America/New_York');
+
+function delete_if_old(jen, job, callback) {
+    jen.job.get(job.name, function(err, data) {
+        if (err) {
+	    console.log('Cannot get Jenkins job ' + job.name);
+	    return callback(null);
+	}
+
+        if (data.actions) {
+	  for (i = 0; i < data.actions.length; i++) {
+	    var def = data.actions[i].parameterDefinitions;
+	    if (def === undefined) continue;
+	    for (j = 0; j < def.length; j++) {
+	      if (def[j].name == 'keep') {
+		if (def[j].defaultParameterValue.value) {
+		  console.log('Keeping ' + job.name);
+		  return callback(null);
+		}
+	      }
+	    }
+	  }
+	}
+
+	// No builds (yet?)
+	if (! data.lastBuild || ! data.lastBuild.number) {
+	    return callback(null);
+	}
+
+	jen.build.get(job.name, data.lastBuild.number, function(err, data) {
+	    if (err) {
+		console.log('Cannot get Jenkins build ' + job.name + ' ' +
+			    data.lastBuild.number);
+		return callback(null);
+	    }
+	    var diff = new Date() - new Date(data.timestamp);
+	    // about three days
+	    if (diff > TIME_LIMIT) {
+		return delete_job(jen, job, callback);
+	    }
+	    return callback(null);
+	});
+    });
+}
+
+function delete_job(jen, job, callback) {
+    jen.job.destroy(job.name, function(err) {
+	if (err) {
+	    console.log('Cannot delete Jenkins job ' + job.name);
+	} else {
+	    console.log('Deleted Jenkins job ' + job.name);
+	}
+	return callback(null);
+    });
+}
+
+module.exports = job;
