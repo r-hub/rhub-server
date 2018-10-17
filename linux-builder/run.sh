9c0d1c6d974aa6b5c4c10d5554f4d6a3a39986fb
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-17 01:01:40 +0100

    Fix internal Linux builder Docker socket
    We need to fix the guid on the socket file,
    within the container. So we start as root,
    chgrp the socket file, and then su to jenkins.

--- linux-builder/run.sh
+++ linux-builder/run.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
-cd ~
+chgrp docker /var/run/docker.sock
 
+cd ~jenkins
+
+cat >jenkins.sh  <<EOF
 java -jar "swarm-client-${SWARM_CLIENT_VERSION}.jar"  \
      -master "${JENKINS_URL}"                         \
      -username "${JENKINS_USER}"                      \
      -passwordEnvVariable JENKINS_PASSWORD            \
      -executors "${RHUB_EXECUTORS}"                   \
      -labels "${JENKINS_LABELS}"
+EOF
+
+chown jenkins:jenkins jenkins.sh
+chmod +x jenkins.sh
+
+su -c ./jenkins.sh jenkins
