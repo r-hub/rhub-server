6a21c1aa9b05a2c545c92c5442146b56d310ec60
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-19 12:18:51 +0100

    Linux script: better temp files & cleanup

--- build-scripts/linux/linux.sh
+++ build-scripts/linux/linux.sh
@@ -9,6 +9,7 @@ main() {
     # do not interfere
     CONTAINER=$(make_uuid | tr -d -- '-')
     CLEANUPIMAGE=
+    CLEANUPFILES=()
     trap cleanup 0
 
     # TODO: parse args
@@ -73,17 +74,21 @@ check_requirements() {
     # Check for R and the packages we need
     if ! R --slave -e 'x <- 1' 2>/dev/null; then
 	>&2 echo "Cannot find R :("
+	>&2 echo "Make sure that R installed and it is in the PATH"
+	>&2 echo "You can install R from https://cran.r-project.org/"
 	return 1
     fi
     if ! R --slave -e 'library(sysreqs)' 2>/dev/null; then
-	>&2 echo "Cannot load the sysreqs package :("
+	>&2 echo "Cannot load the sysreqs package :(  Install it with"
+	>&2 echo "R -q -e \"source('https://install-github.me/r-hub/sysreqs')\""
 	return 2
     fi
 }
 
 download_package() {
     declare -r package="$1"
-    REPLY=$(basename $(mktemp).tar.gz)
+    REPLY=$(mktemp).tar.gz
+    CLEANUPFILES+=("$REPLY")
     if [[ "$package" =~ ^https?:// ]]; then
 	echo ">>>>>==================== Downloading package file"
 	if ! wget -O "$REPLY" "$package"; then
@@ -106,6 +111,7 @@ get_desc() {
     declare -r package="$1"
     local desc=$(tar tzf "$package" | grep "^[^/]*/DESCRIPTION$")
     local dir=$(mktemp -d)
+    CLEANUPFILES+=("$dir")
     tar xzf "$package" -C "$dir" "$desc"
     REPLY="${dir}/${desc}"
 }
@@ -116,12 +122,12 @@ install_sysreqs() {
     local desc=$REPLY
     local cmd="library(sysreqs); cat(sysreq_commands(\"$desc\"))"
     local sysreqs=$(Rscript -e "$cmd")
-    rm -f "$desc"
 
     # Install them, if there is anything to install
     if [[ ! -z "${sysreqs}" ]]; then
 	echo ">>>>>==================== Installing system requirements"
 	local sysreqsfile=$(mktemp)
+	CLEANUPFILES+=("$sysreqsfile")
 	echo "${sysreqs}" > "$sysreqsfile"
 	docker create --user root --name "${container}-1" \
 	       "$image" bash /root/sysreqs.sh
@@ -138,6 +144,7 @@ install_sysreqs() {
 create_env_file() {
     declare -r package="$1" envvars="$2" checkargs="$3"
     local envfile=$(mktemp)
+    CLEANUPFILES+=("$envfile")
 
     # These can be overriden by the user supplied env vars
     echo R_REMOTES_STANDALONE=true              >> "$envfile"
@@ -173,7 +180,8 @@ run_check() {
 
 get_artifacts() {
     declare container="$1"
-    local tmp=$(mktemp -d -p .)
+    local tmp=$(mktemp -d ./tmp.XXXXXXXXX)
+    CLEANUPFILES+=("$tmp")
 
     if ! docker cp "${container}-2:/tmp/artifacts" "$tmp"; then
 	>&2 echo "No artifacts were saved :("
@@ -181,29 +189,35 @@ get_artifacts() {
     fi
 
     local packagename=$(cat "${tmp}/artifacts/packagename")
-    echo $packagename
     local output=${JOB_BASE_NAME:-${packagename}-${container}}
-    echo $output
     if [[ -e "$output" ]]; then
 	local i=1
 	while [[ -e "${output}-${i}" ]]; do i=$((i+1)); done
 	output="${output}-${i}"
     fi
-    echo $output
 
     mv "${tmp}/artifacts" "$output"
-
     echo Saved artifacts in "$output"
 
+    RHUB_ARTIFACTS=${RHUB_ARTIFACTS:-none}
     if [[ "x$RHUB_ARTIFACTS" = "xlocal" ]]; then
-	cp -r ${output} /artifacts/
+	cp -r "$output" /artifacts/
     fi
 }
 
 cleanup() {
+    # Containers
     docker rm -f -v "${CONTAINER}-1" >/dev/null 2>/dev/null || true
     docker rm -f -v "${CONTAINER}-2" >/dev/null 2>/dev/null || true
+
+    # Image
     docker rmi -f   "$CLEANUPIMAGE"  >/dev/null 2>/dev/null || true
+
+    # Temp files
+    for i in "${CLEANUPFILES[@]}"
+    do
+	rm -rf "$i" 2>/dev/null || true
+    done
 }
 
 [[ "$0" == "$BASH_SOURCE" ]] && main "$@"
