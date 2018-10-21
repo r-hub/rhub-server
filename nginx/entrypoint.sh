faf811046d3920c7839e4dc37c9d4c67075d796c
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-21 22:10:01 +0100

    Support HTTPS via secrets

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,13 @@
 
+set
+
 export DOLLAR="$"
-cat /etc/nginx/nginx.conf.in | envsubst | tee /etc/nginx/nginx.conf
+if [ "x$RHUB_HTTPS" == "xtrue" ]; then
+    echo "Installing HTTPS nginx config"
+    cat /etc/nginx/nginx.conf.https.in | envsubst > /etc/nginx/nginx.conf
+else
+    echo "Installing HTTP nginx config"
+    cat /etc/nginx/nginx.conf.in | envsubst > /etc/nginx/nginx.conf
+fi
 
 wait-for frontend:3000 --  nginx -g "daemon off;"
