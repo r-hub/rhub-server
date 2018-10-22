fdb7bafb3b818c88a0cfa86a1377c25490e71426
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-23 00:04:13 +0100

    Better Jenkins proxy, from /jenkins
    No need to open 8080, and an external Jenkins node
    knows the frontend hostname this way. If they are
    not on the same host, the node has to have
    RHUB_BUILDER_URL set up.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,13 +5,9 @@ export DOLLAR="$"
 if [ "x$RHUB_HTTPS" == "xtrue" ]; then
     echo "Installing HTTPS nginx config"
     cat /etc/nginx/nginx.conf.https.in | envsubst > /etc/nginx/nginx.conf
-    cat /etc/nginx/jenkins.conf.https.in | \
-	envsubst > /etc/nginx/conf.d/jenkins.conf
 else
     echo "Installing HTTP nginx config"
     cat /etc/nginx/nginx.conf.in | envsubst > /etc/nginx/nginx.conf
-    cat /etc/nginx/jenkins.conf.in | \
-	envsubst > /etc/nginx/conf.d/jenkins.conf
 fi
 
 wait-for frontend:3000 -- sleep 0
